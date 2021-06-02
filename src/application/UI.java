a79efe386ba4d18200af5aba870e8ce6a633cddd
Marcelino Neto <marcelino.gneto@gmail.com>
2021-06-02 11:22:01 -0300

    Queen

--- src/application/UI.java
+++ src/application/UI.java
@@ -72,7 +72,7 @@ public class UI {
 	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + "  ");
+			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j], false);
 			}
@@ -83,7 +83,7 @@ public class UI {
 	
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + "  ");
+			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j], possibleMoves[i][j]);
 			}
@@ -107,6 +107,7 @@ public class UI {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
+		System.out.print(" ");
 	}
 	
 	private static void printCapturedPieces(List<ChessPiece> captured) {
